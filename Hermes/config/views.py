1292147ab8d7e5aa124fd58a168607f7391a81e8
schaetti <n.schaetti@gmail.com>
2018-09-30 20:55:19 +0200

    Init

---
+++ Hermes/config/views.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ALBUMS
+
+
+def index(request):
+    message = "Salut tout le monde !"
+    return HttpResponse(message)
+# end index
+
+
+def listing(request):
+    albums = ["<li>{}</li>".format(album['name']) for album in ALBUMS]
+    message = """<ul>{}</ul>""".format("\n".join(albums))
+    return HttpResponse(message)
+# end listing
+
+
+def detail(request, album_id):
+    id = int(album_id) # make sure we have an integer.
+    album = ALBUMS[id] # get the album with its id.
+    artists = " ".join([artist['name'] for artist in album['artists']]) # grab artists name and create a string out of it.
+    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album['name'], artists)
+    return HttpResponse(message)
+# end detail
+
+
+def search(request):
+    query = request.GET.get('query')
+    if not query:
+        message = "Aucun artiste n'est demandé"
+    else:
+        albums = [
+            album for album in ALBUMS
+            if query in " ".join(artist['name'] for artist in album['artists'])
+        ]
+
+        if len(albums) == 0:
+            message = "Misère de misère, nous n'avons trouvé aucun résultat !"
+        else:
+            albums = ["<li>{}</li>".format(album['name']) for album in albums]
+            message = """
+                Nous avons trouvé les albums correspondant à votre requête ! Les voici :
+                <ul>
+                    {}
+                </ul>
+            """.format("</li><li>".join(albums))
+
+    return HttpResponse(message)
+# end search
